cbefd2012ced7c5177c22639e47bc011496c31ef
Kseniya Smirnova <fanni@bk.ru>
2022-05-08 11:52:30 +0300

    54

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,75 +1,59 @@
-import Reacr from 'react'
 import { UsersPropsType } from './UsersContainer'
 import s from './users.module.css'
+import axios from 'axios'
+import images from '../../assets/img/images.jpg'
+import React from 'react'
 
-export let Users = (props: UsersPropsType) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                fullName: 'Petya',
-                pfotoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl2D_MvhwlH_AQ5R3EsHaqBjsnJmsbaNg7Yrx1fIuQ&s',
-                followed: false,
-                status: 'I am a boss',
-                location: { city: 'Moscow', country: ' Russia' }
-            },
-            {
-                id: 2,
-                fullName: 'Vasiya',
-                pfotoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl2D_MvhwlH_AQ5R3EsHaqBjsnJmsbaNg7Yrx1fIuQ&s',
-                followed: true,
-                status: 'I am a boss',
-                location: { city: 'Pskov', country: ' Russia' }
-            },
-            {
-                id: 3,
-                fullName: 'Olya',
-                pfotoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl2D_MvhwlH_AQ5R3EsHaqBjsnJmsbaNg7Yrx1fIuQ&s',
-                followed: false,
-                status: 'I am a boss',
-                location: { city: 'Yekaterinburg', country: 'Russia' }
-            },
-        ]
-        )
+
+export class Users extends React.Component {
+          componentDidMount(){
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                 // @ts-ignore
+            this.props.setUsers(response.data.items)
+            });
+        }
+    render() {
+        return <div>
+                {
+                     // @ts-ignore
+            this.props.users.map(u => {
+                <div key={u.id}>
+                    <span>
+                        <div>
+                            <img src={u.photos.small != null ? u.photos.small : images} alt="photo" className={s.photo} />
+                        </div>
+                        <div>
+                            {u.followed
+                             // @ts-ignore
+                                ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                // @ts-ignore
+                                : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>
+                            }
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>
+                                {u.name}
+                            </div>
+                            <div>
+                                {u.status}
+                            </div>
+                        </span>
+                        <span>
+                            <div>
+                                {'u.location.country'}
+                            </div>
+                            <div>
+                                {'u.location.city'}
+                            </div>
+                        </span>
+                    </span>
+                </div>
+            })
+        }
+    </div>
+       
     }
-    return (
-        <div>
-            {
-                props.users.map(u => {
-                    <div key={u.id}>
-                        <span>
-                            <div>
-                                <img src={u.pfotoUrl} alt="photo" className={s.photo} />
-                            </div>
-                            <div>
-                                {u.followed
-                                    ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                    : <button onClick={() => { props.follow(u.id) }}>Follow</button>
-                                }
-                            </div>
-                        </span>
-                        <span>
-                            <span>
-                                <div>
-                                    {u.fullName}
-                                </div>
-                                <div>
-                                    {u.status}
-                                </div>
-                            </span>
-                            <span>
-                                <div>
-                                    {u.location.country}
-                                </div>
-                                <div>
-                                    {u.location.city}
-                                </div>
-                            </span>
-                        </span>
-                    </div>
-                })
-            }
-        </div>
-    )
-
 }
