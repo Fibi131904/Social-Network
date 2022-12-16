00cb4664ff8c9bad41b773da7396ab83c77659f8
Kseniya Smirnova <fanni@bk.ru>
2022-12-16 16:45:48 +0300

    feat: webSocket

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,51 +1,84 @@
-import { Button } from 'antd'
-import React from 'react'
+import { Button, message } from 'antd'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import userPhoto from '../../assets/img/userPhoto.jpg'
+import styles from './ChatPage.module.css';
 
- const ChatPage:React.FC=()=>{
-  return <div>
-<Chat/>
-
-  </div>
+const wsChannal= new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+export type ChatMessageType = {
+message: string
+photo: string
+userId: number
+userName: string
 }
+ const ChatPage: React.FC = () => {
+   return (
+     <div>
+       <Chat />
+     </div>
+   )
+ }
 
-const Chat:React.FC=()=>{
+const Chat: React.FC = () => {
  
-  return <div>
-   <Messages/>
-   <AddMessageForm/>
-  </div>
+  return (
+    <div>
+      <Messages />
+      <AddMessageForm />
+    </div>
+  )
 }
 
-const Message:React.FC=()=>{
-  const message={
-    photo:userPhoto,
-    autor: 'Kseniya',
-    text: 'Hello friends!'
-  }
-  return <div>
-    <img src={message.photo}/><b>{message.autor}</b>
-    <br/>
-    {message.text}
-    <br/>
-  </div>
+const Message: React.FC<{message:ChatMessageType}> = ({message}) => {
+  
+  
+    return (
+    <div>
+      <img src={message.photo} className={styles.messagePhoto} />
+      <b>{message.userName}</b>
+      <br />
+      {message.message}
+      <hr />
+    </div>
+  )
 }
 
-const Messages:React.FC=()=>{
-  const messages=[1,2,3,4]
-  return <div>
-  {messages.map((m:any)=><Message/>)}
-  {messages.map((m:any)=><Message/>)}
-  {messages.map((m:any)=><Message/>)}
-  </div>
+const Messages: React.FC = () => {
+  const [messages, setMessages] = useState<ChatMessageType[]>([])
+  useEffect(() => {
+    wsChannal.addEventListener('message', (e:MessageEvent) => {
+     let newMessages= JSON.parse(e.data)
+      setMessages((prefMessages)=>[...prefMessages, ... newMessages])
+    })
+  }, [])
+
+  return (
+    <div className={styles.messagesBlock}>
+      {messages.map((m, index) => (
+        <Message key={index} message={m} />
+      ))}
+     
+    </div>
+  )
 }
 
 const AddMessageForm:React.FC=()=>{
+  const[message,setMessage]=useState('')
+const sendMessage=()=>{
+  if(!message){
+    return
+  }
+  wsChannal.send(message)
+  setMessage('')
+}
+const onChangeMessageClick=(e:ChangeEvent<HTMLTextAreaElement>)=>{
+setMessage(e.currentTarget.value)
+
+}
   return <div>
    <div>
-    <textarea></textarea>
+    <textarea onChange={onChangeMessageClick}value={message}></textarea>
    </div>
-   <Button>Send</Button>
+   <Button onClick={sendMessage}>Send</Button>
   </div>
 }
 
